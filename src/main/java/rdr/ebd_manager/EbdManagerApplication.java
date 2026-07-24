164c56c2a79c6cd27561a2582c074518d4b5a767
rdr <rdrsilva00@gmail.com>
2026-07-24 14:04:10 -0300

    commit inicial

---
+++ src/main/java/rdr/ebd_manager/EbdManagerApplication.java
@@ -0,0 +1,13 @@
+package rdr.ebd_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbdManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbdManagerApplication.class, args);
+	}
+
+}
